e8ebcbd5d4d8029013a3e9a0a7439bcd4b5ce9d2
Elmerex <humbersber@gmail.com>
2022-09-22 18:15:02 -0300

    init

---
+++ js/app.js
@@ -0,0 +1,55 @@
+/* 
+//1
+document.write("<h1>Hello word</h1>"
+
+//1
+myHTML.innerHTML="<h1>Hello words</h1>"*/
+
+
+/* 
+
+//3
+let a=3
+let b=5
+
+myHTML.innerHTML=("<h1>" + (a+b) + "</h1>") */
+
+/*//4
+let nombre = prompt("Escriba un nombre:")
+
+myHTML.innerHTML=(`<h1>` + "Hola " + nombre + `</h1>`)*/
+
+/*
+//8
+
+let nro1 =prompt("Ingrese un numero:")
+
+let result = (nro1%2)
+
+if (result ==0) myHTML.innerHTML=(`<h1>` + "Es divisible por 2" + `</h1>`)
+
+else
+myHTML.innerHTML=(`<h1>` + "No es divisible por 2" + `</h1>`)*/
+
+/* 
+//6
+var nro1 =prompt("Ingrese un numero:")
+let nro2 =prompt("Ingrese otro numero:")
+
+if (nro1 > nro2) {myHTML.innerHTML= (`<h1>` + "El primer numero es mayor" + `</h1>`)}
+else {myHTML.innerHTML= (`<h1>` + "El Segundo mumero es mayor" + `</h1>`) } */
+
+/* //7
+var nro1 =prompt("Ingrese un numero:")
+var nro2 =prompt("Ingrese otro numero:")
+var nro3 =prompt("Ingrese otro numero:")
+
+if ((nro1 > nro2)&(nro1 > nro3)) 
+{myHTML.innerHTML= (`<h1>` + nro1 + " es el numero mayor" + `</h1>`)}
+
+else if ((nro2 > nro1) & (nro2 > nro3))
+{myHTML.innerHTML= (`<h1>` + nro2 + " es el numero mayor" + `</h1>`)}
+
+else {myHTML.innerHTML= (`<h1>` + nro3 + " es el numero mayor" + `</h1>`)} */
+
+
